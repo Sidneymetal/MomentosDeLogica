11bdea91a091580c57fb6d2307c2c9835bf06291
sidney alexandre <sidneymetalop@gmail.com>
2022-11-16 19:31:05 -0300

    Entregando exercício cronometrado.

---
+++ 16-11/algoritmo.js
@@ -0,0 +1,10 @@
+var salario, filhos, nome
+
+
+nome = (prompt("Digite seu nome:"))
+salario = parseFloat(prompt("Digite o salário:"))
+filhos = parseInt(prompt("Digite a quantidade de filhos:"))
+
+
+console.log(nome, salario, filhos)
+
